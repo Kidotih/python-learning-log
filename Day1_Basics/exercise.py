f249494f843f7b7b85aef0b1cf63d1afab69726f
Kidotih <95543927+Kidotih@users.noreply.github.com>
2025-10-23 18:37:27 +0300

    Completed exercises for day 1

--- Day1_Basics/exercise.py
+++ Day1_Basics/exercise.py
@@ -1,1 +1,10 @@
-# Day 1: Python Basics
+#variables and string formatting
+name ="kidotih"
+age =25
+print(f"hello {name} your age is {age}")
+
+#simple arithmetics
+num1 = 10
+num2 = 5
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}")
